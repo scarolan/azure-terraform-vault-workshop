a14c2ec27b18d7f4530608c63f944b6c7ba8d145
Sean Carolan <scarolan@hashicorp.com>
2019-02-03 20:54:49 -0600

    Clean up outputs

--- outputs.tf
+++ outputs.tf
@@ -7,20 +7,21 @@
 # use an underscore _ to move things to the bottom. In this example we're 
 # providing instructions to the user on how to connect to their own custom 
 # demo environment.
+#
 # output "Vault_Server_URL" {
 #   value = "http://${azurerm_public_ip.vault-pip.fqdn}:8200"
 # }
 # output "MySQL_Server_FQDN" {
 #   value = "${azurerm_mysql_server.mysql.fqdn}"
 # }
-# output "Instructions" {
+# output "_Instructions" {
 #   value = <<SHELLCOMMANDS
-# ##############################################################################
+#
 # # Connect to your Linux Virtual Machine
 # #
 # # Run the command below to SSH into your server. You can also use PuTTY or any
 # # other SSH client. Your password is: ${var.admin_password}
-# ##############################################################################
+# 
 # ssh ${var.admin_username}@${azurerm_public_ip.vault-pip.fqdn}
 # SHELLCOMMANDS
 # }
